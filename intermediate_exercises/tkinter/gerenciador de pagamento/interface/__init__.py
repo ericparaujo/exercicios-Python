cf81703f2e90dcd9787af7fbdc96f59e7a3c69f7
Eric Araujo <lorderic.cg@gmail.com>
2025-07-22 17:42:37 -0300

    Update __init__.py

--- intermediate_exercises/tkinter/gerenciador de pagamento/interface/__init__.py
+++ intermediate_exercises/tkinter/gerenciador de pagamento/interface/__init__.py
@@ -3,28 +3,32 @@ import tkinter
 
 def janela_principal():
     janela = tkinter.Tk()
-    janela.geometry('640x480')
-    janela.title('GERENCIADOR DE PAGAMENTO')
+    janela.geometry('800x600')
+    janela.title(f'GERENCIADOR DE PAGAMENTO'.center(200))
 
-    janela.grid_columnconfigure(0, weight=1)
-    janela.grid_columnconfigure(1, weight=1)
+    tkinter.Label(janela,
+                  text = exibir_textos()['titulo'],
+                  fg = 'green',
+                  font = estilos_fontes()['titulo']).pack(pady=40)
 
-    coluna1 = tkinter.Frame(janela, bg = 'blue')
-    coluna2 = tkinter.Frame(janela, bg = 'red')
 
-    coluna1.grid(row=0, column=0, sticky='nesw')
-    coluna2.grid(row=0, column=1, sticky='nesw')
-
-    label1 = tkinter.Label(coluna1, text='texto 1')
-    label1.pack(pady=40)
-    label2 = tkinter.Label(coluna2, text='texto 2' )
-    label2.pack(pady=40)
 
     janela.mainloop()
 
 
+def estilos_fontes():
+    return {
+        'titulo':('arial', 18, 'bold'),
+        'texto1':('arial', 12, 'bold'),
+        'texto2':('arial', 12),
+        'botao':('arial', 14, 'bold')
+    }
 
 
+def exibir_textos():
+    return{
+        'titulo':'Gerenciador de pagamento'.center(50).upper()
+    }
 
-
-janela_principal()
+if __name__ == '__main__':
+    janela_principal()
